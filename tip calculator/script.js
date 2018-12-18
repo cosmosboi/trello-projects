470e416a302fd2f47f0f90289a78649620ba5318
Bojan Krstevski <bbokilastname@yahoo.com>
2018-12-18 23:52:21 +0100

    Changed formula

--- tip calculator/script.js
+++ tip calculator/script.js
@@ -1,8 +1,10 @@
 function calculate() {
-	let bill = document.querySelector("#billAmount").value
-	let people = document.querySelector("#people").value
-	let result = bill/people;
-	document.querySelector("#rezultat").innerHTML = `Each person has to pay ${result}$`;
-
-
+	
+	let bill = document.querySelector("#billAmount").value;
+	let people = document.querySelector("#people").value;
+	let service = document.querySelector("#select").value;
+	let result = (bill/people) + (bill*service/100);
+	if(bill > 0 && people > 1 ){
+	document.querySelector("#rezultat").innerHTML = `Each person has to pay ${Math.round(result*100)/100}$`;
+       }
 }
